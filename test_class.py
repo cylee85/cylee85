b1efbdabfc5b194eb15e469fc2db878e663c2e72
cylee85 <68527940+cylee85@users.noreply.github.com>
2021-10-20 14:39:30 +0900

    test_class.py added
    for sub_class.py

---
+++ test_class.py
@@ -0,0 +1,18 @@
+class A:
+    def __init__(self):
+        print("A")
+
+
+class B:
+    def __init__(self):
+        print("B")
+
+
+class C:
+    def __init__(self):
+        print("C")
+
+
+class D:
+    def __init__(self):
+        print("D")
